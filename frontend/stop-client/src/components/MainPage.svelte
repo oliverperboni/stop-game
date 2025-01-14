2a9e2cc9758549b9c70f5fc4834257fec3c580e6
oliverperboni <oliverperboni@gmail.com>
2025-01-14 21:03:56 +0000

    feat:my first svelte componet

---
+++ frontend/stop-client/src/components/MainPage.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let inputText = "Oliver";
+  function logInput() {
+    console.log(inputText);
+  }
+</script>
+
+<input type="text" bind:value={inputText} />
+<button on:click={logInput}>Create game</button>
+
---
+++ frontend/stop-client/src/lib/components/Main.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let inputText = "";
+  function logInput() {
+    console.log(inputText);
+  }
+</script>
+
+<input type="text" value={inputText} />
+<button on:click={() => logInput()}>Create game</button>
