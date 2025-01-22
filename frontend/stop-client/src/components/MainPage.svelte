ef39fbb5ea38b5745b12a817b4494a88ca5cd4bf
oliverperboni <oliverperboni@gmail.com>
2025-01-22 19:09:46 +0000

    feat: pseudo routing

--- frontend/stop-client/src/components/MainPage.svelte
+++ frontend/stop-client/src/components/MainPage.svelte
@@ -11,6 +11,7 @@
   let roomId = $state("");
   let player = $state("");
   let alredySubmit = false;
+  let url = $state("home-page");
 
   function addCategory() {
     inputCategories.push(catg);
@@ -29,6 +30,8 @@
       .then((data) => {
         roomId = data.roomId;
       });
+
+    url = "join-room";
   }
   async function getRoom() {
     await fetch(baseUrl + "/room", {
@@ -38,6 +41,9 @@
       .then((data) => {
         console.log(data);
       });
+  }
+  function navigateToCreateGame() {
+    url = "create-room";
   }
 
   function joinGame() {
@@ -53,6 +59,7 @@
         categories: cols,
       }));
     });
+    url = "start-game";
   }
   ws.on("started", (letter, gameId) => {
     store.update((curr) => ({
@@ -78,6 +85,7 @@
 
   function startGame() {
     ws.play(roomId);
+    url = "play-game";
   }
 
   function submitAnswer() {
@@ -96,7 +104,135 @@
   class="min-h-screen bg-gray-900 text-gray-100 flex flex-col justify-center items-center p-4"
 >
   <!-- FORM CATEGORIA -->
-  <div class="bg-gray-800 p-4 rounded-xl shadow-lg">
+  {#if url === "home-page"}
+    <div
+      class="min-h-screen bg-gray-900 text-gray-100 flex flex-col justify-center items-center p-6"
+    >
+      <div class="bg-gray-800 p-8 rounded-xl shadow-lg text-center max-w-lg">
+        <h1 class="text-4xl font-bold text-purple-500 mb-4">
+          Welcome to Stop - OTE!
+        </h1>
+        <p class="text-gray-200 mb-6">
+          A fun and fast-paced word game where creativity and quick thinking
+          lead you to victory. Gather your friends, create a room, and play!
+        </p>
+        <p class="text-gray-400">
+          In "Stop Game," you'll be given a letter and categories to fill in
+          with unique answers before time runs out. Can you beat the competition
+          and come up with the most creative answers?
+        </p>
+
+        <button
+          onclick={navigateToCreateGame}
+          class="w-full p-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 focus:outline-none mt-2"
+        >
+          Create a new game
+        </button>
+      </div>
+    </div>
+  {/if}
+
+  {#if url === "create-room"}
+    <div class="bg-gray-800 p-4 rounded-xl shadow-lg m-1">
+      <div class="flex flex-box justify-center m-2">
+        {#each inputCategories as item}
+          <p class="text-white mt-2">{item}</p>
+        {:else}
+          <p class="text-gray-400 mt-2">No categories</p>
+        {/each}
+      </div>
+
+      <input
+        type="text"
+        bind:value={catg}
+        placeholder="Category"
+        class="w-full p-2 mb-4 bg-gray-700 text-white border-2 border-gray-600 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+      />
+      <button
+        onclick={addCategory}
+        class="w-full p-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 focus:outline-none mb-2"
+      >
+        Add Category
+      </button>
+
+      <button
+        onclick={createRoom}
+        class="w-full p-2 mb-4 bg-purple-600 text-white rounded-md hover:bg-purple-700 focus:outline-none"
+      >
+        Create a Game
+      </button>
+    </div>
+  {/if}
+
+  {#if url === "join-room"}
+    <div class="bg-gray-800 p-6 rounded-xl shadow-lg">
+      <input
+        type="text"
+        bind:value={roomId}
+        placeholder="Room ID"
+        class="w-full p-2 mb-4 bg-gray-700 text-white border-2 border-gray-600 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+      />
+      <input
+        type="text"
+        bind:value={player}
+        placeholder="Player Name"
+        class="w-full p-2 mb-4 bg-gray-700 text-white border-2 border-gray-600 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+      />
+      <button
+        onclick={joinGame}
+        class="w-full p-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 focus:outline-none"
+      >
+        Join Room
+      </button>
+
+      {#if roomId}
+        <h4 class="text-white mt-4">{roomId}</h4>
+      {/if}
+    </div>
+  {/if}
+
+  {#if url === "start-game"}
+    <button
+      onclick={startGame}
+      class="mt-6 p-4 bg-purple-700 text-white rounded-lg hover:bg-purple-800 focus:outline-none"
+    >
+      Start Game
+    </button>
+  {/if}
+  {#if url === "play-game"}
+    <div class="bg-gray-800 p-6 rounded-xl shadow-lg">
+      {#if $store.gameStatus === "in-progress"}
+        <h3 class="text-white mb-4">{$store.letter}</h3>
+        {#each $store.categories as category, index}
+          <div class="mb-4">
+            <p class="text-gray-200">{category}</p>
+            <input
+              type="text"
+              bind:value={inputAnswers[index]}
+              placeholder={`Input ${category}`}
+              class="w-full p-2 mt-2 bg-gray-700 text-white border-2 border-gray-600 rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+            />
+          </div>
+        {/each}
+        <button
+          onclick={submitAnswer}
+          class="w-full p-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 focus:outline-none"
+        >
+          Submit
+        </button>
+      {/if}
+    </div>
+  {/if}
+
+  <!-- <div class="bg-gray-800 p-4 rounded-xl shadow-lg m-1">
+    <div class="flex flex-box justify-center m-2">
+      {#each inputCategories as item}
+        <p class="text-white mt-2">{item}</p>
+      {:else}
+        <p class="text-gray-400 mt-2">No categories</p>
+      {/each}
+    </div>
+
     <input
       type="text"
       bind:value={catg}
@@ -105,28 +241,21 @@
     />
     <button
       onclick={addCategory}
-      class="w-full p-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 focus:outline-none"
+      class="w-full p-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 focus:outline-none mb-2"
     >
       Add Category
     </button>
 
-    {#each inputCategories as item}
-      <p class="text-white mt-2">{item}</p>
-    {:else}
-      <p class="text-gray-400 mt-2">No categories</p>
-    {/each}
-  </div>
-
-  <!-- CREATE AND JOIN -->
-  <div class="bg-gray-800 p-6 rounded-xl shadow-lg">
     <button
       onclick={createRoom}
       class="w-full p-2 mb-4 bg-purple-600 text-white rounded-md hover:bg-purple-700 focus:outline-none"
     >
       Create a Game
     </button>
-    
+  </div> -->
 
+  <!-- CREATE AND JOIN -->
+  <!-- <div class="bg-gray-800 p-6 rounded-xl shadow-lg">
     <input
       type="text"
       bind:value={roomId}
@@ -146,21 +275,13 @@
       Join Room
     </button>
 
-   
     {#if roomId}
       <h4 class="text-white mt-4">{roomId}</h4>
     {/if}
-  </div>
-  <button
-  onclick={startGame}
-  class="mt-6 p-4 bg-purple-700 text-white rounded-lg hover:bg-purple-800 focus:outline-none"
->
-  Start Game
-</button>
-
+  </div> -->
 
   <!-- PLAY THE GAME -->
-  <div class="bg-gray-800 p-6 rounded-xl shadow-lg">
+  <!-- <div class="bg-gray-800 p-6 rounded-xl shadow-lg">
     {#if $store.gameStatus === "in-progress"}
       <h3 class="text-white mb-4">{$store.letter}</h3>
       {#each $store.categories as category, index}
@@ -181,9 +302,8 @@
         Submit
       </button>
     {/if}
-  </div>
+  </div> -->
 </div>
-
 
 <!-- 
 {#each inputCategories as item}
